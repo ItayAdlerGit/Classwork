08a7b65728f361e484a1fe651ce29902f7068788
Itay Adler <115868542+ItayAdlerGit@users.noreply.github.com>
2022-10-15 08:58:06 +0000

    test

---
+++ main.py
@@ -0,0 +1,108 @@
+#c1_e1
+print("computer\nscience")
+#c1_e2
+print("new")
+print("****")
+print("york")
+#c1_e3
+print("Sara\nRivka\nRachel\nLeah")
+#c1_e4
+print("scientific\nand\ntechnological\nreserve")
+#c1_e5
+def main():
+  print("Phython programming language")
+  print("I can code")
+  print("Coding is fun")
+main()
+#c1_e6
+def main2():
+  print("your account balance is:\n5338.45")
+main2()
+#c1_e7
+def main3():
+  print("fourteen\n+\nfive\n=\nnineteen")
+main3()
+#c1_e8
+print("  ##############")
+print("  ##############")
+print("  ###")
+print("  ###")
+print("  ##############")
+print("  ##############")
+print("  ###        ###")
+print("  ###        ###")
+print("  ##############")
+print("  ##############")
+#c1_e9
+def main4():
+  print("First name:Itay\nLast name:Adler\nAddress:alterman12\nCity:Netanya\nSchool:Alterman")
+main4()
+#c1_e10
+def main5():
+  print("**","****","******","I can code!","******","****","**", sep="\n")
+main5()
+#c1_e11
+def main6():
+  print("The Ministry of Education")
+  print("The\nMinistry\nof\nEducation")
+  print("Health")
+main6()
+#c1_e12
+def main7():
+  print("Kfar\nSaba")
+  print("********\nKfarMala\n********")
+  print("Kfar" ,end ="")
+  print("Rosh HaNikra")
+main7()
+#c1_e13 = mp3 streaming compact disc cassete record
+#c1_e14
+def school1():
+  print("classroom")
+  print("teachers lounge")
+def main8():
+  print ("Hagolam School")
+  school1()
+main8()
+#c1_e15
+#Answer = hat shirt pants
+#c1_16
+#Answer = DVD video8 DV laser disc SD Blu-ray Disc VHS Beta VCD
+#c1_17
+def display_1():
+ print("Itzhak")
+def display_2():
+ print("Avraham")
+def display_3():
+ print("Yaakov")
+def main9():
+ display_1()
+ display_2()
+ display_3()
+main9()
+#c1_e18
+def write():
+ print("Happy Holydays!!")
+def main10():
+ write()
+ print("Happy new year!")
+main10()
+#c1_e19
+def school2():
+ print("computer science")
+ print("mathematics")
+def main11():
+ print("biology")
+ school2()
+ print("history")
+main11()
+#c1_e20
+def transportation():
+ print("car")
+ print("track")
+ print("motorcycle")
+def main12():
+ print("train")
+ print("van")
+ print("bus")
+ transportation()
+main12()
